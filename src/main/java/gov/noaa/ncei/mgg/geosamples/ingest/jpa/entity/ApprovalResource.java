85a51fc08e53ba4b1adae50763c625da7c24521e
Chris Slater <6004777+electricsam@users.noreply.github.com>
2025-04-15 10:55:33 -0600

    use open-source version of entity utils

--- src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/jpa/entity/ApprovalResource.java
+++ src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/jpa/entity/ApprovalResource.java
@@ -1,8 +1,9 @@
 package gov.noaa.ncei.mgg.geosamples.ingest.jpa.entity;
 
 import edu.colorado.cires.cmg.jpa.model.EntityWithId;
+import java.io.Serializable;
 
-public interface ApprovalResource<I> extends EntityWithId<I> {
+public interface ApprovalResource<I extends Serializable> extends EntityWithId<I> {
   GeosamplesApprovalEntity getApproval();
   void setApproval(GeosamplesApprovalEntity approval);
 
